2b7c4c40a994f722ac00750e7c0a8fb1e3f10204
Aekarin Jujaibun <150250748+AkkarinJB@users.noreply.github.com>
2025-02-27 02:34:29 +0700

    Develop Brach (#1)
    * #1 Add logo website
    
    * #1 Config routing
    
    * #1 Add about component
    
    * #1 Add contact component
    
    * #1 Config routing
    
    * #1 Add font style
    
    * #1 Edit header html
    
    * #1 Edit home component
    
    * #1 Add portfoli component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SkillsComponent } from './skills/skills.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {path: 'home' , component: HomeComponent},
+    {path: 'about' , component: AboutComponent},
     {path: 'skills' , component: SkillsComponent},
+    {path: 'portfolio' , component: PortfolioComponent},
+    {path: 'contact' , component: ContactComponent},
+
 ];
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-content',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
